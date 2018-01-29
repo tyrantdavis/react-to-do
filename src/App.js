14c44884d7f0ff573deea57fba948b7716565395
Tyrant Davis <davistyrant@gmail.com>
2018-01-28 23:03:07 -0600

    Create event handler as class method on App component

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ class App extends Component {
       ]
     };
   }
+
+  toggleComplete() {
+    console.log('toggleComplete executed');
+  }
+
   render() {
     return (
       <div className="App">
