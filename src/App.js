51f477e4491813a958374bf2ed69ac972aad2ebc
Tyrant Davis <davistyrant@gmail.com>
2018-01-29 01:53:40 -0600

    Set value of text into this.state.newTodoDescription...

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
           )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
-          <input type="text" value={ this.state.newToDoDescription }/>
+          <input type="text" value={ this.state.newTodoDescription }/>
           <input type="submit" />
         </form>
       </div>
